821a5197847a07ac5d9ff452b72962a397dcd263
poleumdev <64774219+poleumdev@users.noreply.github.com>
2020-05-11 09:54:01 +0200

    Add files via upload

---
+++ lang/en/block_escapecell.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_escapecell
+ * @category    string
+ * @copyright   marc.leconte
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Score Escape Cell';
+$string['escapecell:addinstance'] = 'Add an instance of the Score Escape Cell block';
+$string['escapecell:myaddinstance'] = 'Add an instance of the Score Escape Cell block';
+$string['privacy:metadata'] = 'The Escape Cell Block plugin doesn\'t record any personal data.';
+$string['totscore'] = 'Student results';
+$string['titparticip'] = 'List of non-participants';
+$string['collevel'] = 'Level';
+$string['colbonus3'] = '3 Bonus';
+$string['colbonus2'] = '2 Bonus';
+$string['colbonus1'] = '1 Bonus';
+$string['colnobonus'] = 'No Bonus';
+$string['colnoplay'] = 'Unplayed';
+$string['titnbstudent'] = 'Number of students enrolled';
+$string['titpodium'] = 'Hall of fame';
+$string['escapecell_max_level'] = 'Number of Escape Cell levels';
+$string['escapecell_max_level_desc'] = 'Indicate the maximum number of EscapeCell levels present in a course.';
+$string['hidescore'] = 'Hide the score';
+$string['stdnivnotpassed'] = 'Students who have not completed the level {$a}';
---
+++ lang/fr/block_escapecell.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_escapecell
+ * @category    string
+ * @copyright   marc.leconte
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Score Escape Cell';
+$string['escapecell:addinstance'] = 'Ajouter une instance du bloc Score Escape Cell';
+$string['escapecell:myaddinstance'] = 'Ajouter une instance du bloc Score Escape Cell';
+$string['privacy:metadata'] = 'Le plugin Bloc d\'Escape Cell n\'enregistre aucune donnée personnelle.';
+$string['totscore'] = 'Résultats des étudiants';
+$string['titparticip'] = 'Liste des non participants';
+$string['collevel'] = 'Niveau';
+$string['colbonus3'] = '3 Bonus';
+$string['colbonus2'] = '2 Bonus';
+$string['colbonus1'] = '1 Bonus';
+$string['colnobonus'] = 'Sans Bonus';
+$string['colnoplay'] = 'Non fait';
+$string['titnbstudent'] = 'Nombre d\'étudiant inscrit';
+$string['titpodium'] = 'Tableaux des scores';
+$string['escapecell_max_level'] = 'Nombre de niveau Escape Cell';
+$string['escapecell_max_level_desc'] = 'Indiquer le nombre maximun de niveau EscapeCell présent dans un cours.';
+$string['hidescore'] = 'Cacher le score';
+$string['stdnivnotpassed'] = 'Etudiants n\'ayant pas fait le niveau {$a}';
